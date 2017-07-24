9b3485d6805513dcdea08a98201f1127274b0fb4
pamela415 <prcruz10303@yahoo.com>
2017-07-23 20:23:02 -0700

    preload

---
+++ preload.js
@@ -0,0 +1,20 @@
+var preload = {
+  
+    preload: function (){
+        var loadingBar = this.add.sprite(160, 240, "loading");
+        console.log("loaded loading bar");
+        loadingBar.anchor.setTo(0.5, 0.5);
+        this.load.setPreloadSprite(loadingBar);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    }
+};
